bbc062ab01e8ae2032e2493816cb1ef570ec2caf
Ransford Sarpong <ransarptech2570@gmail.com>
2025-07-02 04:15:41 +0000

    Initial commit

---
+++ CommandLineDemo.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+
+public class CommandLineDemo{
+	public static void main(String[] args) {
+			String[] fruits ={"Apple","Mango","Orange"};
+			for(int i =0; i<fruits.length;i++){
+				System.out.println(fruits[i]);
+				
+				Scanner sc = new  Scanner(System.in);
+				sc.nextLine();
+				
+
+			} 
+
+	
+			
+	}
+}
---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+
+
+
+
+public  class Hello{
+  public static void main(String[] args) {
+    
+    
+    int [] numbers = {1,2,3,4,5,7};
+    System.out.println(numbers.length);
+    System.out.println(Arrays.toString(numbers));
+    
+
+  }
+}
