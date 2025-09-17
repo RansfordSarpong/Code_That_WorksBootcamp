8dfd174a70616262bda3006400ce5b6891e4e96d
Ransford Sarpong <ransarptech2570@gmail.com>
2025-09-17 17:22:00 +0000

    Initial commit:MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,17 @@
+public class MyProfile{
+
+  public static void main(String[] args) {
+      String  Myname = "Reggie Walsh";
+      int Myage = 20;
+      String Favourite_ProLanguage = "Python";
+
+      String city = "New York city";
+
+      System.out.println("Hello,my name is "+Myname);
+      System.out.println("I am "+Myage +"years old.");
+      System.out.println("I live in "+city);
+      System.out.println("My favourite programming language is "+Favourite_ProLanguage);
+
+
+  }
+}
